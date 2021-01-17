f8dd66e6f2442562f00cc0d2e3c6d0a25693a260
m.morshinin <m.morshinin@b2b-center.ru>
2021-01-17 04:03:05 +0300

    main input form

--- resources/js/vue/app.vue
+++ resources/js/vue/app.vue
@@ -1,11 +1,56 @@
 <template>
     <div class="todoListContainer">
-        Hello
+        <div class="heading">
+            <h2 id="title">Todo List</h2>
+            <add-item-form />
+        </div>
+        <list-view :items="items" />
     </div>
 </template>
 
 <script>
+import addItemForm from "./addItemForm";
+import listView from "./listView";
 export default {
-
+    components: {
+        addItemForm,
+        listView
+    },
+    data: function () {
+        return {
+            items: []
+        }
+    },
+    methods: {
+        getList() {
+            axios.get('api/items')
+            .then(response => {
+                this.items = response.data
+            })
+            .catch(error => {
+                console.log(error);
+            })
+        }
+    },
+    created() {
+        this.getList();
+    }
 }
 </script>
+
+<style scoped>
+.todoListContainer {
+    width: 350px;
+    margin: auto;
+}
+
+.heading {
+    background: #e6e6e6;
+    padding: 10px;
+}
+
+#title {
+    text-align: center;
+}
+
+</style>
